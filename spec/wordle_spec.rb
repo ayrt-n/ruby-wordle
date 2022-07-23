3f75f9587cc0ba97bb57a3d88cfdacdb3765dcfe
ayrton <ayrton.parkinson1@gmail.com>
2022-07-23 10:22:06 -0400

    Add variant to #check_guess test

--- spec/wordle_spec.rb
+++ spec/wordle_spec.rb
@@ -17,11 +17,11 @@ describe Wordle do
     end
 
     it 'highlights letter yellow when the letter exists but in the wrong position' do
-      guess = 'MUST'
-      expected_results = ['M'.bg_grey.black,
-                          'U'.bg_grey.black,
-                          'S'.bg_green.black,
-                          'T'.bg_green.black]
+      guess = 'STOP'
+      expected_results = ['S'.bg_yellow.black,
+                          'T'.bg_yellow.black,
+                          'O'.bg_grey.black,
+                          'P'.bg_grey.black]
       expect(wordle.check_guess(guess)).to eql(expected_results)
     end
 
@@ -32,6 +32,16 @@ describe Wordle do
                           ' '.bg_grey.black,
                           ' '.bg_grey.black]
       expect(wordle.check_guess(guess)).to eql(expected_results)
+    end
+
+    it 'highlights letters all three colors when it should' do
+      complex_wordle = Wordle.new('QQQWWEQQWERTTYUUIIOP')
+      guess = 'QWQQW  QQQUAASRUI PP'
+      expected_results = ['Q'.bg_green.black, 'W'.bg_yellow.black, 'Q'.bg_green.black, 'Q'.bg_yellow.black, 'W'.bg_green.black,
+                          ' '.bg_grey.black, ' '.bg_grey.black, 'Q'.bg_green.black, 'Q'.bg_yellow.black, 'Q'.bg_grey.black,
+                          'U'.bg_yellow.black, 'A'.bg_grey.black, 'A'.bg_grey.black, 'S'.bg_grey.black, 'R'.bg_yellow.black,
+                          'U'.bg_green.black, 'I'.bg_green.black, ' '.bg_grey.black, 'P'.bg_grey.black, 'P'.bg_green.black]
+      expect(complex_wordle.check_guess(guess)).to eql(expected_results)
     end
   end
 end
