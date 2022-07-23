9bd7f89d7f63d270e69b7998905c7053a54746a4
ayrton <ayrton.parkinson1@gmail.com>
2022-07-23 09:37:15 -0400

    Create basic set of tests for #check_guess

--- spec/wordle_spec.rb
+++ spec/wordle_spec.rb
@@ -1,7 +1,37 @@
 # frozen_string_literal: true
 
 require './lib/wordle'
+require './lib/colorize_string'
 
 describe Wordle do
-  
+  let(:wordle) { Wordle.new('TEST') }
+
+  describe '#check_guess' do
+    it 'highlights letter green when the letter exists in the right position' do
+      guess = 'TEST'
+      expected_results = ['T'.bg_green.black,
+                          'E'.bg_green.black,
+                          'S'.bg_green.black,
+                          'T'.bg_green.black]
+      expect(wordle.check_guess(guess)).to eql(expected_results)
+    end
+
+    it 'highlights letter yellow when the letter exists but in the wrong position' do
+      guess = 'MUST'
+      expected_results = ['M'.bg_grey.black,
+                          'U'.bg_grey.black,
+                          'S'.bg_green.black,
+                          'T'.bg_green.black]
+      expect(wordle.check_guess(guess)).to eql(expected_results)
+    end
+
+    it 'highlights letter gren when the letter does not exist' do
+      guess = '    '
+      expected_results = [' '.bg_grey.black,
+                          ' '.bg_grey.black,
+                          ' '.bg_grey.black,
+                          ' '.bg_grey.black]
+      expect(wordle.check_guess(guess)).to eql(expected_results)
+    end
+  end
 end
