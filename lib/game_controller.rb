81c21793bfb3e2ebf32a1680e34e1421f54b5695
ayrton <ayrton.parkinson1@gmail.com>
2022-07-23 10:44:07 -0400

    Add initialize and #win?

--- lib/game_controller.rb
+++ lib/game_controller.rb
@@ -1,6 +1,19 @@
 # frozen_string_literal: true
 
+require './lib/wordle'
+
 # Class representation of Wordle gameflow
 class GameController
-  
+  attr_reader :word, :number_of_rounds
+  attr_accessor :round
+
+  def initialize(word, round = 0, number_of_rounds = 5)
+    @word = Wordle.new(word)
+    @round = round
+    @number_of_rounds = number_of_rounds
+  end
+
+  def win?(guess)
+    word.equal?(guess)
+  end
 end
