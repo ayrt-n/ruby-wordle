f43629edecfdd021faa73c56906d2cb5c2d60ffa
ayrton <ayrton.parkinson1@gmail.com>
2022-07-23 10:44:29 -0400

    Add tests for #win?

--- spec/game_controller_spec.rb
+++ spec/game_controller_spec.rb
@@ -3,4 +3,15 @@
 require './lib/game_controller'
 
 describe GameController do
+  let(:game) { GameController.new('TEST') }
+
+  describe '#win?' do
+    it 'returns true if provided the correct word' do
+      expect(game.win?('TEST')).to be true
+    end
+
+    it 'returns false if provided the incorrect word' do
+      expect(game.win?('NOPE')).to be false
+    end
+  end
 end
